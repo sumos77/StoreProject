79a3436e6a1b1ff9e34bdfc2c3ac914b5684ee52
Martin <48153829+sumos77@users.noreply.github.com>
2021-03-12 22:46:34 +0100

    Receipt added

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -45,10 +45,11 @@ namespace StoreProject
 
         //Instance variables to be reached and changed in all methods in this class
         private StackPanel cartStack;
+        private StackPanel receiptProductPanel;
         private TextBox rebateCodeTextBox;
         private Label sumPurchaseLabel;
         private decimal rebateMultiplier;
-        private Rebate currentRebateCode = null;
+        private Rebate currentRebateCode;
 
         public MainWindow()
         {
@@ -65,6 +66,7 @@ namespace StoreProject
             //Loads the cart at startup if it exists
             //Cart = LoadCart();
             rebateMultiplier = 1;
+            currentRebateCode = null;
 
             // Window options
             Title = "Butik";
@@ -133,6 +135,8 @@ namespace StoreProject
             Grid.SetRow(confirmPurchaseButton, 0);
             Grid.SetColumn(confirmPurchaseButton, 2);
 
+            confirmPurchaseButton.Click += ConfirmPurchaseButton_Click;
+
             Grid rebateCodeGrid = new Grid();
             rebateCodeGrid.RowDefinitions.Add(new RowDefinition { Height = GridLength.Auto });
             rebateCodeGrid.ColumnDefinitions.Add(new ColumnDefinition());
@@ -173,7 +177,181 @@ namespace StoreProject
             sumPurchaseGrid.Children.Add(sumPurchaseLabel);
             Grid.SetRow(sumPurchaseLabel, 0);
             Grid.SetColumn(sumPurchaseLabel, 0);
+
+            //#################################################
+            // KVITTO
+            // ################################################
+            receiptProductPanel = new StackPanel
+            {
+                Orientation = Orientation.Vertical
+
+            };
+            mainStack.Children.Add(receiptProductPanel);
+
         }
+
+        private void ConfirmPurchaseButton_Click(object sender, RoutedEventArgs e)
+        {   
+            if(receiptProductPanel.Children.Count > 0)
+            {
+                receiptProductPanel.Children.Clear();
+            }
+            DrawReceipt();
+            Cart.Clear();
+            DrawCart();
+        }
+
+        private void DrawReceipt()
+        {
+            Grid.SetRow(receiptProductPanel, 0);
+            Grid.SetColumn(receiptProductPanel, 0);
+
+            #region Header Grid
+            Grid receiptHeaderGrid = new Grid();
+            receiptHeaderGrid.RowDefinitions.Add(new RowDefinition());
+            receiptHeaderGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptHeaderGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptHeaderGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptHeaderGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptProductPanel.Children.Add(receiptHeaderGrid);
+
+            Label receiptNameLabel = CreateBorderdLabel("Produkt");
+            receiptHeaderGrid.Children.Add(receiptNameLabel);
+            Grid.SetRow(receiptNameLabel, 0);
+            Grid.SetColumn(receiptNameLabel, 0);
+            Label receiptDescriptionLabel = CreateBorderdLabel("Antal");
+            receiptHeaderGrid.Children.Add(receiptDescriptionLabel);
+            Grid.SetRow(receiptDescriptionLabel, 0);
+            Grid.SetColumn(receiptDescriptionLabel, 1);
+            Label receiptPriceLabel = CreateBorderdLabel("Styckpris");
+            receiptHeaderGrid.Children.Add(receiptPriceLabel);
+            Grid.SetRow(receiptPriceLabel, 0);
+            Grid.SetColumn(receiptPriceLabel, 2);
+            Label receiptTotPriceLabel = CreateBorderdLabel("TotalPris");
+            receiptHeaderGrid.Children.Add(receiptTotPriceLabel);
+            Grid.SetRow(receiptTotPriceLabel, 0);
+            Grid.SetColumn(receiptTotPriceLabel, 3);
+            #endregion Header
+
+            foreach (KeyValuePair<Product, int> entry in Cart)
+            {
+                string productName = entry.Key.Name;
+                string productPrice = entry.Key.Price;
+                int quantity = entry.Value;
+                receiptProductPanel.Children.Add(creatReceiptProductGrid(productName, quantity, int.Parse(productPrice)));
+            }
+
+            #region Receipt Sum
+
+            Grid receiptSumGrid = new Grid();
+            receiptSumGrid.Margin = new Thickness(0, 5, 0, 0);
+            receiptSumGrid.RowDefinitions.Add(new RowDefinition());
+            receiptSumGrid.RowDefinitions.Add(new RowDefinition());
+            receiptSumGrid.RowDefinitions.Add(new RowDefinition());
+            receiptSumGrid.RowDefinitions.Add(new RowDefinition());
+            receiptSumGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptSumGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptProductPanel.Children.Add(receiptSumGrid);
+
+            #region Receipt Colum 0
+            Label recepitRebateCodeLabel = CreateBorderdLabel("Rabattkod");
+            receiptSumGrid.Children.Add(recepitRebateCodeLabel);
+            Grid.SetRow(recepitRebateCodeLabel, 0);
+            Grid.SetColumn(recepitRebateCodeLabel, 0);
+            Label receiptSumLabel = CreateBorderdLabel("Summa");
+            receiptSumGrid.Children.Add(receiptSumLabel);
+            Grid.SetRow(receiptSumLabel, 1);
+            Grid.SetColumn(receiptSumLabel, 0);
+            Label rebateCodeLabel = CreateBorderdLabel("Rabatt");
+            receiptSumGrid.Children.Add(rebateCodeLabel);
+            Grid.SetRow(rebateCodeLabel, 2);
+            Grid.SetColumn(rebateCodeLabel, 0);
+            Label receiptRebateSumLabel = CreateBorderdLabel("Summa efter rabat");
+            receiptSumGrid.Children.Add(receiptRebateSumLabel);
+            Grid.SetRow(receiptRebateSumLabel, 3);
+            Grid.SetColumn(receiptRebateSumLabel, 0);
+            #endregion Receipt Colum 0
+
+            #region Receipt Colum 1
+
+            decimal sumTotalWithRebate = TotalSum();
+            //Label recepitRebateCodeLabel = CreateBorderdLabel(rebateCodeTextBox.Text);
+            Label recepitRebateCode = new Label();
+            if (currentRebateCode != null)
+            {
+                recepitRebateCode = CreateBorderdLabel(currentRebateCode.Code);
+            }
+            else
+            {
+                recepitRebateCode = CreateBorderdLabel("Ingen rabattkod");
+            }
+            receiptSumGrid.Children.Add(recepitRebateCode);
+            Grid.SetRow(recepitRebateCode, 0);
+            Grid.SetColumn(recepitRebateCode, 1);
+            //Vet inte hur jag skulle göra på denna så alla efter kommer att utgå från denna
+            Label receiptSum = CreateBorderdLabel("12233" + "Kr");
+            receiptSumGrid.Children.Add(receiptSum);
+            Grid.SetRow(receiptSum, 1);
+            Grid.SetColumn(receiptSum, 1);
+            //Tog 20% som ett exempel
+            Label rebateCode = CreateBorderdLabel(((int.Parse("123") * 5) * 0.20).ToString() + "kr" + "(20%)");
+            receiptSumGrid.Children.Add(rebateCode);
+            Grid.SetRow(rebateCode, 2);
+            Grid.SetColumn(rebateCode, 1);
+            Label receiptRebateSum = CreateBorderdLabel(sumTotalWithRebate.ToString() + "kr");
+            receiptSumGrid.Children.Add(receiptRebateSum);
+            Grid.SetRow(receiptRebateSum, 3);
+            Grid.SetColumn(receiptRebateSum, 1);
+            #endregion Receipt Colum 1
+            #endregion Receipt Sum
+        }
+
+        public static Grid creatReceiptProductGrid(string productName, int quantity, int price)
+        {
+            Grid receiptProductGrid = new Grid();
+            receiptProductGrid.RowDefinitions.Add(new RowDefinition());
+            receiptProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            receiptProductGrid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            Label productNameLabel = CreateBorderdLabel(productName);
+            receiptProductGrid.Children.Add(productNameLabel);
+            Grid.SetRow(productNameLabel, 0);
+            Grid.SetColumn(productNameLabel, 0);
+
+            Label receiptquantityLabel = CreateBorderdLabel(quantity.ToString());
+            receiptProductGrid.Children.Add(receiptquantityLabel);
+            Grid.SetRow(receiptquantityLabel, 0);
+            Grid.SetColumn(receiptquantityLabel, 1);
+
+            Label receiptPriceLabel = CreateBorderdLabel(price.ToString());
+            receiptProductGrid.Children.Add(receiptPriceLabel);
+            Grid.SetRow(receiptPriceLabel, 0);
+            Grid.SetColumn(receiptPriceLabel, 2);
+
+            Label recepitTotPriceLabel = CreateBorderdLabel((price * quantity).ToString());
+            receiptProductGrid.Children.Add(recepitTotPriceLabel);
+            Grid.SetRow(recepitTotPriceLabel, 0);
+            Grid.SetColumn(recepitTotPriceLabel, 3);
+
+
+            return receiptProductGrid;
+        }
+        public static Label CreateBorderdLabel(string header)
+        {
+            Label label = new Label
+            {
+                Content = header,
+                HorizontalContentAlignment = HorizontalAlignment.Left,
+                Margin = new Thickness(0, 0, 0, 0),
+                Padding = new Thickness(2),
+                BorderBrush = Brushes.Black,
+                BorderThickness = new Thickness(1)
+            };
+            return label;
+        }
+
 
         private void SaveCartButton_Click(object sender, RoutedEventArgs e)
         {
@@ -510,6 +688,20 @@ namespace StoreProject
         }
 
         private decimal TotalSum()
+        {
+            decimal sum = 0;
+
+            foreach (KeyValuePair<Product, int> entry in Cart)
+            {
+                decimal productPrice = decimal.Parse(entry.Key.Price);
+                int quantity = entry.Value;
+                sum += productPrice * quantity;
+            }
+
+            return sum;
+        }
+
+        private decimal TotalSumWithRebate()
         {
             decimal sum = 0;
 
